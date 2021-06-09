f930212d7012f72fd9ad4e8efcd97e419f75b4dc
MuhammadAleem366 <muhammadaleem366@gmail.com>
2021-06-09 11:00:32 +0500

    Custom Error Pages added

--- MVCApp/Controllers/HomeController.cs
+++ MVCApp/Controllers/HomeController.cs
@@ -16,6 +16,7 @@ namespace MVCApp.Controllers
 
         public ActionResult About()
         {
+            throw new Exception();
             ViewBag.Message = "Your application description page.";
 
             return View();
